2a42f117360baba00ae1a14aeb0cca31f953f1d6
bigalplayer <b4u_ia@outlook.pt>
2014-02-04 17:27:10 +0000

    added portable class library/phone and tablet projects in healthcontrol vs13 solution

---
+++ HealthControl.VS13/HealthControl.Phone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HealthControl.Phone.Resources;
+
+namespace HealthControl.Phone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
